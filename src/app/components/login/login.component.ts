2cb962609edf6d66bc4b5a87ac6a9ddd05ba11e4
HomeBjo <lala2-wuppertal@hotmail.com>
2024-06-14 17:14:44 +0200

    $*

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,15 +6,32 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  email:string = '';
-  password:string = '';
+  username: string = '';
+  password: string = '';
 
+  login() {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Cookie', 'csrftoken=YBh8sJSVxSduX8YzoE1Xdm8gLvvM76vx');
 
-  
-login(){
+    const raw = JSON.stringify({
+      username: 'bjoern',
+      password: '123456',
+    });
 
-}
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    fetch('http://127.0.0.1:8000/login/', requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  }
 }
